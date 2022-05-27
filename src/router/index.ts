3ba41b7230a01011d31057db785bb2c1604a8e4c
lwp2070809 <lwp2070809@gmail.com>
2022-05-27 06:43:42 +0000

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,7 +27,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHashHistory('/game-pass-values/'),
     routes
 })
 
